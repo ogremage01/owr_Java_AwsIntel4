eece2c1fba8d95b7c5cf7952296167092efad511
ogremage01 <curselife01@gmail.com>
2025-01-05 14:56:47 +0900

    게임 로직 수정

--- JavaExamTest_OWR/src/hk/exam/ten/GameLogic.java
+++ JavaExamTest_OWR/src/hk/exam/ten/GameLogic.java
@@ -6,24 +6,31 @@ import java.util.Scanner;
 
 public class GameLogic {
 
+	private int input = 0;
+	private int userAnswer = 0;
+	private int min = 2;
+	private int max = 11;
+	private int nowPlayer = 0;
+	private int tryTime = 0;
+	private int up = 0;
+	private int down = 0;
+
 	public void upAndDown() {
 
 		BottleCap bc = new BottleCap();
 		ArrayList<Player> playerList = new ArrayList<Player>();
 
 		Scanner sc = new Scanner(System.in);
-		int input = 0;
-		int userAnswer = 0;
-		int min = 2;
-		int max = 11;
 
 		while (true) {
 			try {
 				while (true) {
-					System.out.println("참가할 인원을 말씀해 주세요.(" + min + "~" + max + "인)");
+					System.out.println("참가할 인원을 말씀해 주세요.(" + min + "~" + max + "인)");// 참가 플레이어 입력
 					input = sc.nextInt();
 					if (input >= min && input <= max) {
 						break;
+					} else {
+						System.out.println("범위 내 숫자만 입력가능합니다.");// 최소/최대 예외 처리
 					}
 				}
 
@@ -32,65 +39,67 @@ public class GameLogic {
 
 			} catch (InputMismatchException ie) {
 				// TODO: handle exception
-				System.out.println("다시 입력해주세요");
+				System.out.println("잘못 입력하셨습니다. 숫자만 입력가능합니다. 다시 입력해 주십시오.");// 문자 입력시 예외 처리
 				sc.next();
 				continue;
 			}
 
 		}
-
+		// 참가자 이름 입력
 		for (int i = 0; i < input; i++) {
 
 			String name = "";
 
 			System.out.println(i + "번째 참가자의 이름을 말씀해 주세요.");
 			name = sc.nextLine();
-			Player pc = new Player(name);
+			Player pc = new Player();
+			pc.setName(name);
 			playerList.add(pc);
 
 		}
+		// 병뚜껑 숫자 결정 및 초기 업&다운 수치 결정
 		bc.setNum((int) (Math.random() * 1000) + 1);
-		int up = (bc.getNum() / 100 * 100) + 100;
-		int down = bc.getNum() / 100 * 100;
-		System.out.println(bc.getNum());
-		int nowPlayer = 0;
-		int tryTime = 0;
+		up = (bc.getNum() / 100 * 100) + 100;
+		down = bc.getNum() / 100 * 100;
+		System.out.println(bc.getNum());// 테스트용 점수표기
 
-		while (true) {
+		while (true) {// 수 입력 진행
 			tryTime++;
 
 			while (true) {
 				try {
-					System.out.println(playerList.get(nowPlayer).name + "님");
+					System.out.println(playerList.get(nowPlayer).getName() + "님");
 
-					System.out.println(down + "~" + up + "사이의 숫자를 입력해 주세요");
+					System.out.println(down + "~" + up + "숫자를 입력해주세요");
 					userAnswer = sc.nextInt();
 					break;
 
-				} catch (InputMismatchException ie) {
+				} catch (InputMismatchException ie) {// 문자 입력 시
 					// TODO: handle exception
-					System.out.println("숫자를 입력해 주세요");
+					System.out.println("잘못 입력하셨습니다. 숫자만 입력가능합니다. 다시 입력해 주십시오.");
 					sc.next();
 					continue;
 				}
 			}
 
-			if (userAnswer == bc.getNum()) {
-				playerList.get(nowPlayer).passCheck = true;
-				System.out.println(playerList.get(nowPlayer).name + "님");
+			if (userAnswer == bc.getNum()) {// 정답 적중 시
+				playerList.get(nowPlayer).setPassCheck();
+				System.out.println(playerList.get(nowPlayer).getName() + "님");
 				System.out.println("병뚜껑 번호" + bc.getNum() + "맞췄습니다.");
 				System.out.println("총 시도 횟수는 " + tryTime + "번입니다.");
+
 				for (int i = 0; i < playerList.size(); i++) {
-					if (playerList.get(i).passCheck) {
-						System.out.println("병뚜껑 숫자를 맞추신 분은 [name=" + playerList.get(i).name + "입니다., userAnswer="
-								+ userAnswer + " , passCheck=" + playerList.get(i).passCheck + "] 벌칙받으러 ㄱㄱ^^");
-					} else if (playerList.get(i).passCheck == false) {
-						System.out.println("[name=" + playerList.get(i).name + "님은 생존하셨습니다. , passCheck="
-								+ playerList.get(i).passCheck + "] 축하합니다~~");
+					if (playerList.get(i).getPassCheck()) {
+						System.out.println("병뚜껑 숫자를 맞추신 분은 [name=" + playerList.get(i).getName() + "입니다., userAnswer="
+								+ userAnswer + " , passCheck=" + playerList.get(i).getPassCheck() + "] 벌칙받으러 ㄱㄱ^^");
+					} else if (playerList.get(i).getPassCheck() == false) {
+						System.out.println("[name=" + playerList.get(i).getName() + "님은 생존하셨습니다. , passCheck="
+								+ playerList.get(i).getPassCheck() + "] 축하합니다~~");
 					}
 				}
 
 				return;
+				// 범위 이내 오답 시
 			} else if (userAnswer > bc.getNum() && userAnswer <= up) {
 				System.out.println("틀렸습니다. 다음 분은 더 작은 수를 입력해 주세요");
 				up = userAnswer - 1;
@@ -99,8 +108,9 @@ public class GameLogic {
 			} else if (userAnswer < bc.getNum() && userAnswer >= down) {
 				System.out.println("틀렸습니다. 다음 분은 더 큰 수를 입력해 주세요");
 				down = userAnswer + 1;
-				nowPlayer = (nowPlayer + 1) % playerList.size();
+				nowPlayer = (nowPlayer + 1 % playerList.size());
 
+				// 범위 바깥 오답 시
 			} else if (userAnswer >= up) {
 				System.out.println(up + "보다 큰 값을 넣을 수 없습니다.");
 			} else if (userAnswer <= down) {
--- JavaExamTest_OWR/src/hk/exam/ten/Player.java
+++ JavaExamTest_OWR/src/hk/exam/ten/Player.java
@@ -1,17 +1,31 @@
 package hk.exam.ten;
 
 public class Player {
-	
-	String name = "";//이름
-	boolean passCheck = false;
-	
-	
-	Player(){
-		
+
+	private String name = "";// 이름
+	private boolean passCheck = false;
+
+	Player() {
+
 	}
-	Player(String name){
-		this.name=name;
+
+	Player(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
+	public boolean getPassCheck() {
+		return passCheck;
+	}
+	public void setPassCheck() {
+		this.passCheck=!this.passCheck;
+	}
 
 }
